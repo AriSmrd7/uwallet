7b1abb7304574f281e94a909200a3992e39613a5
AriSmrd7 <arismrd13@gmail.com>
2022-10-14 00:56:25 +0700

    update

--- src/screens/Wallet.js
+++ src/screens/Wallet.js
@@ -5,7 +5,47 @@ import HeaderNavbar from '../components/HeaderNavbar'
 const Wallet = () => {
     return (
         <div>
-            <HeaderNavbar/>
+            <HeaderNavbar />
+            <div className='container'>
+                <p>Balance</p>
+                <h1>Rp 27,230,069</h1>
+                <h3>AriSmrd Wallet</h3>
+                <div className='content'>
+                    <h4 className='content-title'>Your assets</h4>
+                    <div class="card-layout">
+                        <div class="card-layout__item">
+                            Utopium
+                            <div class="card-balance__item">
+                                2,000,000 UTP
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-layout">
+                        <div class="card-layout__item">
+                            Bitcoin
+                            <div class="card-balance__item">
+                                0.0723 BTC
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-layout">
+                        <div class="card-layout__item">
+                            Ethereum
+                            <div class="card-balance__item">
+                                0.68 ETH
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-layout">
+                        <div class="card-layout__item">
+                            Binance Coin
+                            <div class="card-balance__item">
+                                1.956 BNB
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <BottomNavBar name='wallet' />
         </div>
     )
